2f9203c751e9cc9dcae8258bb37c7eaf7923933d
VictorHOgawa <victor.ogawa@gmail.com>
2025-08-06 14:07:59 -0300

    chore(other): add clarity

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,6 +20,15 @@ export default function RootLayout({
 }>) {
   return (
     <html>
+      <head>
+        <Script id="ms_clarity" strategy="afterInteractive">
+          {`(function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "sqowyw6hiq");`}
+        </Script>
+      </head>
       <body className={`${dmSans.className}`}>
         {/* Google Tag Manager */}
         <Script
